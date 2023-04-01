0a303fc012a387a7d9aec608e9b6a497ff7dde9e
John Marcos <johnsaguay@gmail.com>
2023-03-31 20:00:25 -0400

    added css code to make web app responsive

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -12,14 +12,14 @@ function Portfolio () {
                 <Row>
                     <Col>
                         <div className='justify-content-center'>
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/gQJ6H5Pb7Og?start=3" title="YouTube video player" 
+                            <iframe className='vids' width="560" height="315" src="https://www.youtube.com/embed/gQJ6H5Pb7Og?start=3" title="YouTube video player" 
                                     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;web-share" allowfullscreen>
                             </iframe>
                         </div>
                     </Col>
                     <Col>
                         <div>
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/xluWCztKaQw?start=30" title="YouTube video player" 
+                            <iframe className='vids' width="560" height="315" src="https://www.youtube.com/embed/xluWCztKaQw?start=30" title="YouTube video player" 
                                     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
                             </iframe>
                         </div>
